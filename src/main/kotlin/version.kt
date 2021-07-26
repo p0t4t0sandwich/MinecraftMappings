b3c07f53bd7ed9ed0587158234aad51b9639e364
Mgazul <mgazul@163.com>
2021-07-26 12:58:41 +0800

    Delete yarn

--- src/main/kotlin/version.kt
+++ src/main/kotlin/version.kt
@@ -15,38 +15,37 @@ enum class MinecraftVersion(
     val mcVersion: String,
     val mcpVersion: String? = null,
     val mcpConfig: Boolean = false,
-    val yarn: Boolean = false,
     val spigot: Boolean = false
 ) {
-    V1_17("1.17", "snapshot_20210610", true, false, true),
-    V1_16_5("1.16.5", "snapshot_20210115", true, false, true),
-    V1_16_4("1.16.4", "snapshot_20201102", true, false, true),
-    V1_16_3("1.16.3", "snapshot_20201025", true, false, true),
-    V1_16_3_YARN("1.16.3", null, false, true, true),
-    V1_16_2("1.16.2", "snapshot_20200812", true, false, true),
-    V1_16_2_YARN("1.16.2", null, false, true, true),
-    V1_16_1_YARN("1.16.1", null, false, true, true),
-    V1_16_1("1.16.1", "snapshot_20200625", false, true, true),
-    V1_15_2("1.15.2", "snapshot_20200515", true, false, true),
-    V1_15_1("1.15.1", "snapshot_20191217", true, false, true),
-    V1_15("1.15", "snapshot_nodoc_20191212", true, false, true),
-    V1_14_4("1.14.4", "snapshot_nodoc_20190719", true, false, true),
-    V1_14_3("1.14.3", "snapshot_nodoc_20190719", true, true, true),
-    V1_14_2("1.14.2", "stable_nodoc_53", false, true, true),
-    V1_14_1("1.14.1", "stable_nodoc_51", false, true, true),
-    V1_14("1.14", "stable_nodoc_49", false, true, true),
-    V1_13_2("1.13.2", "stable_nodoc_47", false, false, true),
-    V1_13_1("1.13.1", "stable_nodoc_45", false, false, true),
-    V1_13("1.13", "stable_nodoc_43", false, false, true),
-    V1_12_2("1.12.2", "snapshot_nodoc_20171003", true, false, true),
-    V1_12("1.12", "snapshot_nodoc_20180814", false, false, true),
-    V1_11("1.11", "snapshot_nodoc_20170612", false, false, true),
-    V1_10_2("1.10.2", "snapshot_nodoc_20160703", false, false, true),
-    V1_9_4("1.9.4", "snapshot_nodoc_20160604", false, false, true),
-    V1_9("1.9", "snapshot_nodoc_20160516", false, false, true),
-    V1_8_8("1.8.8", "snapshot_nodoc_20151216", false, false, true),
-    V1_8("1.8", "snapshot_nodoc_20141130", false, false, true),
-    V1_7_10("1.7.10", "snapshot_nodoc_20140925", true, false, false);
+    V1_17("1.17.1", "snapshot_20210706", true, true),
+    V1_16_5("1.16.5", "snapshot_20210115", true, true),
+    V1_16_4("1.16.4", "snapshot_20201102", true, true),
+    V1_16_3("1.16.3", "snapshot_20201025", true, true),
+    V1_16_3_YARN("1.16.3", null, false, true),
+    V1_16_2("1.16.2", "snapshot_20200812", true, true),
+    V1_16_2_YARN("1.16.2", null, false, true),
+    V1_16_1_YARN("1.16.1", null, false, true),
+    V1_16_1("1.16.1", "snapshot_20200625", false, true),
+    V1_15_2("1.15.2", "snapshot_20200515", true, true),
+    V1_15_1("1.15.1", "snapshot_20191217", true, true),
+    V1_15("1.15", "snapshot_nodoc_20191212", true, true),
+    V1_14_4("1.14.4", "snapshot_nodoc_20190719", true, true),
+    V1_14_3("1.14.3", "snapshot_nodoc_20190719", true, true),
+    V1_14_2("1.14.2", "stable_nodoc_53", false, true),
+    V1_14_1("1.14.1", "stable_nodoc_51", false, true),
+    V1_14("1.14", "stable_nodoc_49", false, true),
+    V1_13_2("1.13.2", "stable_nodoc_47", false, true),
+    V1_13_1("1.13.1", "stable_nodoc_45", false, true),
+    V1_13("1.13", "stable_nodoc_43", false, true),
+    V1_12_2("1.12.2", "snapshot_nodoc_20171003", true, true),
+    V1_12("1.12", "snapshot_nodoc_20180814", false, true),
+    V1_11("1.11", "snapshot_nodoc_20170612", false, true),
+    V1_10_2("1.10.2", "snapshot_nodoc_20160703", false, true),
+    V1_9_4("1.9.4", "snapshot_nodoc_20160604", false, true),
+    V1_9("1.9", "snapshot_nodoc_20160516", false, true),
+    V1_8_8("1.8.8", "snapshot_nodoc_20151216", false, true),
+    V1_8("1.8", "snapshot_nodoc_20141130", false, true),
+    V1_7_10("1.7.10", "snapshot_nodoc_20140925", true, false);
 
     fun generateMappings(): List<Pair<String, Mappings>> {
         // Mappings, fromObf
@@ -67,10 +66,6 @@ enum class MinecraftVersion(
             val buildDataCommit = getBuildDataCommit(mcVersion)
             val obf2spigotMappings = downloadSpigotMappings(buildDataCommit)
             mappings.add(Pair(obf2spigotMappings, "spigot"))
-        }
-        if (yarn) {
-            val obf2yarnMappingsSet = getYarnMappings(mcVersion)
-            obf2yarnMappingsSet.forEach { (id, m) -> mappings.add(Pair(m, id)) }
         }
 
         val completeMappings = mutableListOf<Pair<String, Mappings>>()
