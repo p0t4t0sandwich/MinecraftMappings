753538aaab3ca43ebf78c2968e0f75101fb23c0b
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2026-03-23 00:01:58 -0600

    Properly fixed 1.14.4 Yarn mappings issues

--- src/main/kotlin/provider/yarn.kt
+++ src/main/kotlin/provider/yarn.kt
@@ -57,20 +57,30 @@ fun getYarnMappings(minecraftVersion: String): Map<String, Mappings> {
                     }
                 }
                 line.startsWith("FIELD\t") -> {
-                    val field = tinyMappings.getField(parts[1], parts[3], parts[2])
-                    parts.forEachIndexed { index, mapped ->
-                        if (index >= 4) {
-                            val namespace = namespaces[index - 4]
-                            if (namespace != "official") {
+                    // 1.14.4 Yarn mappings issues:
+                    // Not real:  FIELD ago D field_19384 field_19384 visitCount
+                    // Is wrong:  FIELD ago J c           field_19384 field_19384
+                    // Should be: FIELD ago J c           field_19384 visitCount
+                    if (minecraftVersion == "1.14.4" && parts[1] == "ago" && parts[2] == "D" && parts[3] == "field_19384") {
+                        println("Skipping field: $parts because it doesn't exist")
+                    } else {
+                        val field = tinyMappings.getField(parts[1], parts[3], parts[2])
 
-                                // val broken = arrayOf("field_24240", "field_24245", "field_19384")
-                                // 1.14.4 yarn fix
-                                val broken = arrayOf("field_19384")
-
-                                if (mapped !in broken) {
-                                    field.add(namespace, mapped)
-                                } else {
-                                	println("Test: $namespace $mapped")
+                        parts.forEachIndexed { index, mapped ->
+                            if (index >= 4) {
+                                val namespace = namespaces[index - 4]
+                                if (namespace != "official") {
+                                    try {
+                                        if (minecraftVersion == "1.14.4" && namespace == "named" && mapped == "field_19384") {
+                                            println("Changing field: $namespace $mapped to visitCount")
+                                            field.add(namespace, "visitCount")
+                                        } else {
+                                            field.add(namespace, mapped)
+                                        }
+                                    } catch (e: Exception) {
+                                        e.printStackTrace()
+                                        println(line)
+                                    }
                                 }
                             }
                         }
